a93b4a375aba7290754fafcd1d6aed5177776520
jaydipjadhav <jaydipdjadhav>
2024-08-22 01:08:17 +0530

    done hero section

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="hero">
+        <img src="/public/bappa3.jpg" alt=""  width= "400px" className="hero-image" />
+        <div className="hero-content">
+    <h1 >Hostelcha <b>Raja</b></h1>
+    <hr />
+    <p>Celebrating Ganpati with my hostel family—because home is where the heart is, and our hearts are with Bappa!</p>
+    <button className="btn">ADD</button>
+  
+   <button className="btn">GET</button>
+ 
+  </div>
+ 
+      </div>
+       
+    </>
+  )
+}
+
+export default App
